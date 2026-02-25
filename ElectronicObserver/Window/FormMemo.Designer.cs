20f1cbf13d7b0ee61b6f032bfa82554a84e121fa
myi <myi@exaple.com>
2026-02-25 17:12:02 +0900

    コミット漏れ

---
+++ ElectronicObserver/Window/FormMemo.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ElectronicObserver.Window
+{
+	partial class FormMemo
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.txb_Memo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txb_Memo
+            // 
+            this.txb_Memo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txb_Memo.Font = new System.Drawing.Font("ＭＳ ゴシック", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txb_Memo.Location = new System.Drawing.Point(0, 0);
+            this.txb_Memo.Multiline = true;
+            this.txb_Memo.Name = "txb_Memo";
+            this.txb_Memo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txb_Memo.Size = new System.Drawing.Size(300, 200);
+            this.txb_Memo.TabIndex = 1;
+            this.txb_Memo.WordWrap = false;
+            this.txb_Memo.TextChanged += new System.EventHandler(this.TextChange_txb_Memo);
+            // 
+            // FormMemo
+            // 
+            this.AutoHidePortion = 150D;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.txb_Memo);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HideOnClose = true;
+            this.Name = "FormMemo";
+            this.Text = "メモ";
+            this.Load += new System.EventHandler(this.FormLog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox txb_Memo;
+	}
+}
---
+++ ElectronicObserver/Window/FormMemo.cs
@@ -0,0 +1,126 @@
+﻿using ElectronicObserver.Observer;
+using ElectronicObserver.Observer.kcsapi.api_get_member;
+using ElectronicObserver.Observer.kcsapi.api_port;
+using ElectronicObserver.Resource;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using static System.Windows.Forms.LinkLabel;
+
+namespace ElectronicObserver.Window
+{
+
+	public partial class FormMemo : DockContent
+	{
+		private string f_memoValue;
+
+		public FormMemo(FormMain parent)
+		{
+			// 初期化
+			InitializeComponent();
+			// コンフィグ再設定
+			ConfigurationChanged();
+			// フォーム終了時処理追加(memo内容保存用);
+			Application.ApplicationExit += ExitForm;
+		}
+
+		/// <summary>
+		/// フォームロード時処理
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void FormLog_Load(object sender, EventArgs e)
+		{
+			// イベント設定　コンフィグ変更時に設定を反映させてるっぽい
+			Utility.Configuration.Instance.ConfigurationChanged += ConfigurationChanged;
+
+			// アイコン設定
+			Icon = ResourceManager.ImageToIcon(ResourceManager.Instance.Icons.Images[(int)ResourceManager.IconContent.FormEquipmentList]);
+
+			// 前回値読出
+			ReadText();
+		}
+
+		/// <summary>
+		/// コンフィグ変更時処理
+		/// </summary>
+		void ConfigurationChanged()
+		{
+			// フォント反映
+			txb_Memo.Font = Font = Utility.Configuration.Config.UI.MainFont;
+		}
+
+		/// <summary>
+		/// よくわからん
+		/// </summary>
+		/// <returns></returns>
+		protected override string GetPersistString()
+		{
+			return "Memo";
+		}
+
+		/// <summary>
+		/// テキスト保存
+		/// </summary>
+		private void SaveText()
+		{
+			// パス設定
+			string path = System.IO.Path.Combine(Application.StartupPath,"memo.txt");
+			// パスにファイルが存在する場合のみ実行
+			if (File.Exists(path)){
+				// shiftJISで書き込み 上書き
+				using(StreamWriter writer = new StreamWriter(path, false, Encoding.GetEncoding("UTF-8"))){
+					// 書き込み
+					writer.Write(f_memoValue);
+				}
+			}
+		}
+
+		/// <summary>
+		/// テキスト読込
+		/// </summary>
+		private void ReadText()
+		{
+			// パス設定
+			string path = System.IO.Path.Combine(Application.StartupPath,"memo.txt");
+			// パスにファイルが存在する場合のみ実行
+			if (File.Exists(path)){
+				using(var wr = new StreamReader(path)){
+					//　読込反映
+					this.txb_Memo.Text = wr.ReadToEnd();
+				}
+			}
+
+		}
+
+
+		/// <summary>
+		/// フォーム終了時処理
+		/// </summary>
+		private void ExitForm(object sender,EventArgs e)
+		{
+			// テキスト保存
+			SaveText();
+		}
+
+		/// <summary>
+		/// テキスト変更時処理
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void TextChange_txb_Memo(object sender, EventArgs e)
+		{
+			// memo内容を保持　保持用クラスがあっても良い？　
+			// テキストボックス内のテキストはフォーム終了時イベントが発火するときには空になっているので、変数に退避させている
+			f_memoValue = this.txb_Memo.Text;
+		}
+	}
+}
